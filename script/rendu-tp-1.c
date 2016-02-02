c1f37ca11401ece3f4a834d9d2aab1c21fe9b720
alexandre <alexandre.d.info@gmail.com>
2016-02-02 10:06:50 +0100

    script a finir

--- script/rendu-tp-1.c
+++ script/rendu-tp-1.c
@@ -4,9 +4,17 @@
 
 int main(int argc, char *argv[]) {
 
-  printf("Question 1\n");
-
   printf("Question 2\n");
-
+  printf("  java -jar bioseq.jar print-fasta-sequences test1.fasta\n");
+  printf("Question 3\n");
+  printf("  java -jar bioseq.jar print-fasta-stats test1.fasta\n");
+  printf("Question 4\n");
+  printf("  java -jar bioseq.jarlist-kmers 4 test1.fasta\n");
+  printf("Question 5\n");
+  printf("  java -jar bioseq.jar common-kmers 4 test1.fasta test2.fasta\n");
+  printf("Question 9\n");
+  printf("  java -jar bioseq.jar ratio-common-kmers 4 test1.fasta test2.fasta\n");
+  printf("Question 11\n");
+  printf("  java -jar bioseq.jar random-mutations 10 ebola-z.fasta\n");
   return 0;
 }
